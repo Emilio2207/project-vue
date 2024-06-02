fdaa0fe3be75cd5f2abfacafe653e10fd6c44928
EmilioRamirez <emi.emilioramirez@gmail.com>
2024-06-02 18:00:56 -0300

    component-counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hello World!</h1>
+  <p>{{ counter }}</p>
+  <button @click="increase">Increase</button>
+  <button @click="decrease">Decrease</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+const increase = () => {
+  counter.value++;
+};
+const decrease = () => {
+  counter.value--;
+};
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
